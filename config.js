30fc0b9d4490ca16d1ed3738dcfbc194d237f05c
mathbase1 <mathbase1@hotmail.com>
2026-08-18 14:16:00 +0100

    Connect frontend to Vercel backend

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ window.ENGLISH_MARKER_CONFIG = Object.freeze({
   backendBaseUrl:
     window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
       ? window.location.origin
-      : "ff"
+      : "https://english-bcg.vercel.app"
 });
